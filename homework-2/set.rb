d0f9a66d1918434bc7133ec53027ca3eb91e0ec5
klimmen <klim030187@gmail.com>
2014-12-08 22:07:20 +0200

    add set.rb

---
+++ homework-2/set.rb
@@ -0,0 +1,13 @@
+require 'set'
+set_a = Set.new([2,7, 9, 5, 4, 6, 1, 8, 9,10])
+puts "set"
+puts set_a.inspect
+puts "============================="
+puts "set.sort"
+puts set_a.sort.join ","
+puts "============================="
+puts "set.first"
+puts set_a.first
+puts "============================="
+puts "set.count"
+puts set_a.count
