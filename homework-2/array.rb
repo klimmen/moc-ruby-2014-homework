9b227e0318f39db8efcd6dcbb7b9bff3f266fafd
klimmen <klim030187@gmail.com>
2014-12-08 21:02:47 +0200

    add class and def

--- homework-2/array.rb
+++ homework-2/array.rb
@@ -1,3 +1,52 @@
+class Snake
+
+  def sleepingSnake (arraySnake)
+    puts
+    puts "arraySnake"
+    arraySnake.each {|item| item.each {|item2| print item2; print ' '}; puts }
+  end
+
+  def amountCircle(n)
+    if n % 2 == 1
+      n+=1
+    end
+    n= n/2
+  end
+
+
+  def creepSnake (arraySnake, n)
+    i=0
+    puts 
+    puts "result"
+    loop do
+      arraySnake.each_index do |item| 
+        if item == 0
+          print arraySnake[item].join " "
+          print " "
+        elsif item == (arraySnake.length-1)
+          print arraySnake[item].reverse.join " "
+          arraySnake.delete_at(item)
+        else 
+          arraySnake[item].reverse!
+          print arraySnake[item][0]
+          print " "
+          arraySnake[item].delete_at(0) 
+        end 
+      end
+      arraySnake.delete_at(0)
+      print " "
+      arraySnake.reverse.each do |item|
+        item.reverse!
+        print item[0]
+        print " "
+        item.delete_at(0)
+      end
+      i+=1
+      break if i==n
+    end
+  end
+end
+
 #  1  2  3  4 5 
 # 16 17 18 19 6
 # 15 24 25 20 7
@@ -9,45 +58,11 @@
 #array_a = [[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]
 array_a = [[1,2,3,4,5],[16,17,18,19,6],[15,24,25,20,7],[14,23,22,21,8],[13,12,11,10,9]]
 
-puts
-puts "array_a"
-array_a.each {|item| item.each {|item2| print item2; print ' '}; puts }
-n = array_a.length
-if n % 2 == 1
-  n+=1
-end
-n= n/2
-i=0
-puts 
-puts "result"
-loop do
-  array_a.each_index do |item| 
-    if item == 0
-      print array_a[item].join " "
-      print " "
-    elsif item == (array_a.length-1)
-      print array_a[item].reverse.join " "
-      array_a.delete_at(item)
-    else 
-      array_a[item].reverse!
-      print array_a[item][0]
-      print " "
-      array_a[item].delete_at(0) 
-    end 
-  end
-  array_a.delete_at(0)
-  print " "
-  array_a.reverse.each do |item|
-    item.reverse!
-    print item[0]
-    print " "
-    item.delete_at(0)
-  end
-  i+=1
-  break if i==n
-end
-puts
-puts
 
+snake = Snake.new
+snake.sleepingSnake(array_a)
+n = snake.amountCircle(array_a.length)
+snake.creepSnake(array_a,n)
+puts
 
 
