3c81ea8eb493f66d512d3fbb7eb56546604160a2
klimmen <klim030187@gmail.com>
2014-12-14 17:14:10 +0200

    add method have_hobbies?

--- homework-4/1.rb
+++ homework-4/1.rb
@@ -1,4 +1,6 @@
 
+require 'json'
+
 module PersonAdult
   def adult?(*personal_data)
     personal_data.each do |data|
@@ -10,34 +12,40 @@ module PersonAdult
     end
   end
 end
-  
 
-require 'json'
+
+
 RESPONSE='{"person":{"personal_data":{"name": "John Smith", "gender":"male", "age":56}, "social_profiles":["http://facebook.com/lala","http://twitter.com/lala","http://lala.ru"], "additional_info":{"hobby":["pubsurfing","drinking","hiking"], "pets":[{"name":"Mittens","species":"Felis silvestris catus"}]}}}'
 response = JSON.parse(RESPONSE)
+
 Person = Struct.new(*response["person"].keys.collect(&:to_sym))
 Person.class_eval do
   include PersonAdult
+    def have_hobbies?(additional_info)
+  	result = false
+    additional_info.each do |key, value|    	
+    	if key == "hobby" and !value.empty?
+    		result = true
+    	end	
+    end
+    result 
+  end
 end
 
 person = Person.new(*response["person"].values)
 
 person.instance_eval do
   def twitter_account?
-    social_profiles.each do |social_profile|
-      if social_profile.downcase.scan(/twitter/).empty?
-      	return true
-      end
+  	result = false
+    social_profiles.each do |social_profile|     
+     if !social_profile.downcase.scan(/twitter/).empty?
+     	 result = true
+     end
     end
+    result
   end
 end
  
 p person.adult?(person.personal_data)
 p person.twitter_account?
-
-
-#RESPONSE='{"person":{"personal_data":{"name": "John Smith", "gender":"male", "age":56},
-# "social_profiles":["http://facebook./lala","http://twitter/lala","http://lala"],
-# "additional_info":{"hobby":["pubsurfing","drinking","hiking"],
-#  "pets":[{"name":"Mittens","species":"Felis silvestris catus"}]}}}'
-#RESPONSE='{"person":{"personal_data":{"name": "John Smith", "gender":"male", "age":56}, "social_profiles":["http://facebook","http://twitter","http://"], "additional_info":{"hobby":["pubsurfing","drinking","hiking"], "pets":[{"name":"Mittens","species":"Felis silvestris catus"}]}}}'
+p person.have_hobbies?(person.additional_info)
