2b4a0ee14a26bb04598e8f0e9c987708f830b669
klimmen <klim030187@gmail.com>
2014-12-14 20:31:43 +0200

    update method have_hobbies?

--- homework-4/1.rb
+++ homework-4/1.rb
@@ -2,14 +2,17 @@
 require 'json'
 
 module PersonAdult
+
   def adult?(*personal_data)
   	personal_data[0]["age"] >= 18
   end
+
 end
 
 module MagicWords
 
   module ClassMethods
+
     def magic_methods(pets)
       pets.each do |key, value| 
         define_method("pets_#{key}") do 
@@ -17,6 +20,7 @@ module MagicWords
         end
       end 
     end
+
   end
 
   def self.included(base)
@@ -25,8 +29,8 @@ module MagicWords
 
 end
 
-
 RESPONSE='{"person":{"personal_data":{"name": "John Smith", "gender":"male", "age":56}, "social_profiles":["http://facebook.com/lala","http://twitter.com/lala","http://lala.ru"], "additional_info":{"hobby":["pubsurfing","drinking","hiking"], "pets":[{"name":"Mittens","species":"Felis silvestris catus"}]}}}'
+
 response = JSON.parse(RESPONSE)
 
 Person = Struct.new(*response["person"].keys.collect(&:to_sym))
@@ -34,21 +38,19 @@ Person = Struct.new(*response["person"].keys.collect(&:to_sym))
 Person.class_eval do
   include PersonAdult
   include MagicWords
-    def have_hobbies?(additional_info)
-  	  result = false
-      additional_info.each do |key, value|    	
-        if key == "hobby" and !value.empty?
-    	  result = true
-    	end	
-      end
-      result 
-    end
+  
+  def have_hobbies?(additional_info)
+    !additional_info["hobby"].empty?
+  end
+  
   magic_methods(response["person"]["additional_info"] ["pets"][0])
+
 end
 
 person = Person.new(*response["person"].values)
 
 person.instance_eval do
+ 
   def twitter_account?
     result = false
     social_profiles.each do |social_profile|     
@@ -60,6 +62,7 @@ person.instance_eval do
   end
  
 end
+
 p "adult?" 
 p person.adult?(person.personal_data)
 p "twitter_account?"
